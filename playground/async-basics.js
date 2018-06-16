9ce2282e65c3c65304c30b10e4cee5b3b1b48838
JohnSmith <johnsmith@JohnSmiths-iMac.local>
2018-06-16 11:46:44 -0700

    add section4-33

---
+++ playground/async-basics.js
@@ -0,0 +1,10 @@
+console.log('Starting app');
+
+setTimeout(() => {
+    console.log('Inside of callback');
+},2000);
+
+setTimeout(() => {
+    console.log('Second Inside')
+},0);
+console.log('Finishing app');
